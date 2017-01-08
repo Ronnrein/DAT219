c2e45fe2ffec9f791ea824cef56646f236cee3f7
Ronny Reinhardtsen <ronnyr12@uia.no>
2017-01-08 18:43:22 +0000

    Added site

--- hand-in_1/src/script.js
+++ hand-in_1/src/script.js
@@ -1,3 +1,118 @@
-$(document).ready(function(){
 
+function App() {
+    this.searchInputEl = "#search-input";
+    this.searchResultEl = "#search-result";
+    this.widget1El = "#header1-widget";
+    this.steamStatusEl = "#steam-status";
+    this.steamGamesEl = "#steam-games";
+    this.steamPlaytimeEl = "#steam-playtime";
+
+    var self = this;
+
+    init();
+
+    function init() {
+        new WidgetSpectrum(self.widget1El, 10, 500);
+        new WidgetGameSearch(self.searchInputEl, self.searchResultEl, function(steam) {
+            $(self.steamStatusEl).html(steam.isOnline() ? "online" : "offline");
+            $(self.steamGamesEl).html(steam.getGameCount());
+            $(self.steamPlaytimeEl).html(steam.getTotalPlaytime());
+        });
+    }
+}
+
+function WidgetSpectrum(container, bars, interval) {
+    this.container = $(container);
+    this.animationTime = interval;
+
+    var self = this;
+
+    init();
+
+    function init() {
+        for(var i = 0; i < bars; i++) {
+            self.container.append("<div></div>");
+        }
+        animate();
+        setInterval(animate, self.animationTime);
+    }
+
+    function animate() {
+        self.container.children("div").each(function() {
+            var height = Math.floor(Math.random() * (90-10+1) + 10);
+            $(this).stop(true, true).animate({height: height+"%"}, self.animationTime, "linear");
+        });
+    }
+}
+
+function WidgetGameSearch(inputElement, resultElement, callback) {
+    this.steamUserURL = "http://div.ronnrein.com/proxy.php?data=steamUser";
+    this.steamGamesURL = "http://div.ronnrein.com/proxy.php?data=steamGames";
+    this.inputElement = $(inputElement);
+    this.resultElement = $(resultElement);
+
+    var steamUserData;
+    var steamGamesData;
+    var self = this;
+
+    init();
+
+    this.searchForGame = function(searchString) {
+        if(steamGamesData === undefined || searchString === "") {
+            return undefined;
+        }
+        return $.grep(steamGamesData.games, function(item) {
+            return item.name.toLowerCase().indexOf(searchString.toLowerCase()) >= 0;
+        })[0];
+    };
+
+    this.getGameCount = function() {
+        return steamGamesData.game_count;
+    };
+
+    this.getTotalPlaytime = function() {
+        var playtime = 0;
+        $.each(steamGamesData.games, function(i, item) {
+            playtime += item.playtime_forever;
+        });
+        return Math.floor(playtime/60);
+    };
+
+    this.isOnline = function() {
+        return !!steamUserData.personastate;
+    };
+
+    function init() {
+        $.when(
+            $.getJSON(self.steamUserURL, function(data) {
+                steamUserData = data.response.players[0];
+            }),
+            $.getJSON(self.steamGamesURL, function(data) {
+                steamGamesData = data.response;
+            })
+        ).done(function(data1, data2) {
+            steamUserData = data1[0].response.players[0];
+            steamGamesData = data2[0].response;
+            bindEvents();
+            callback(self);
+        });
+    }
+
+    function searchInputChanged() {
+        var game = self.searchForGame(self.inputElement.val());
+        if(game !== undefined) {
+            self.resultElement.html(game.name);
+        }
+        else {
+            self.resultElement.html("");
+        }
+    }
+
+    function bindEvents() {
+        self.inputElement.on("keyup change", searchInputChanged);
+    }
+}
+
+$(document).ready(function(){
+    new App();
 });
